22b14c5d3b794a22bda38b3ce6d93f635c5e5748
amitleiba <amitleiba012@gmail.com>
2022-09-29 21:38:06 +0300

    Adding initLib function

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,15 @@
 #include <pjsua2.hpp>
+#include<iostream>
+
+void initLib(){
+    pj::Endpoint ep;
+    ep.libCreate();
+    pj::EpConfig ep_cfg;
+    ep.libInit(ep_cfg);
+}
 
 int main()
 {
+    initLib();
     return 0;
 }
